83c5d1dfb973d615c3edd433de75a2fdbcb4d5d1
Aleksandra <markovasasha94@gmail.com>
2023-04-08 16:40:03 +0300

    feat: add modal for card items

--- src/components/cardItem/CardItem.tsx
+++ src/components/cardItem/CardItem.tsx
@@ -2,24 +2,15 @@ import { CharacterResult } from '../../types/types';
 import { useState } from 'react';
 import CardModal from '../cardModal/CardModal';
 
-const CardItem = ({ id, image, name, gender, species, location }: CharacterResult) => {
+const CardItem = ({ id, image, name }: CharacterResult) => {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
     <div key={id} className="card__item">
       <img src={image} alt="photo" className="card__img" />
-      <h3 className="card__item-title">{`Plant '${name}'`}</h3>
-      <div className="card__item-main">
-        <div className="card__item-like">{species}</div>
-        <div className="card__item-like">{gender}</div>
-      </div>
-
-      <div className="card__item-about">
-        <p>{location.name}</p>
-      </div>
-      <div className="card__item-about"></div>
+      <h3 className="card__item-title">{name}</h3>
       <button onClick={() => setOpen(true)}>See more</button>
-      {open && <CardModal characterId={id} />}
+      {open && <CardModal characterId={id} setOpen={setOpen} />}
     </div>
   );
 };
--- src/components/cardModal/CardModal.tsx
+++ src/components/cardModal/CardModal.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import getCharacterById from '../../api/getCaracterById';
 import { CharacterResult } from '../../types/types';
 import Preloader from '../preloader/Preloader';
 import './cardModal.css';
 
-const CardModal = (props: { characterId: number }) => {
+const CardModal = (props: {
+  characterId: number;
+  setOpen: React.Dispatch<SetStateAction<boolean>>;
+}) => {
   const [character, setCaracter] = useState<CharacterResult>();
   const [error, setError] = useState<string>('');
 
@@ -12,26 +15,29 @@ const CardModal = (props: { characterId: number }) => {
     getCharacterById(props.characterId)
       .then((character) => setCaracter(character))
       .catch((err) => setError(err.message));
-    console.log(character, error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return character ? (
-    <div className="card__item">
-      <img src={character.image} alt="photo" className="card__img" />
-      <h3 className="card__item-title">{`Plant '${character.name}'`}</h3>
-      <div className="card__item-main">
-        <div className="card__item-like">{character.species}</div>
-        <div className="card__item-like">{character.gender === 'Male' ? '♂' : '♀'}</div>
-      </div>
-      <div className="card__item-about">
-        <p>{character.location.name}</p>
-      </div>
-      <div className="card__item-about"></div>
-    </div>
-  ) : (
-    <div>
-      <strong>{error ? error : <Preloader />}</strong>
+  return (
+    <div className="modal__overlay" onClick={() => props.setOpen(false)}>
+      {character ? (
+        <div className="card__modal" onClick={(e) => e.stopPropagation()}>
+          <img src={character.image} alt="photo" className="card__img" />
+          <h3 className="card__item-title">{character.name}</h3>
+          <div className="card__item-main">
+            <div className="card__item-like">{character.species}</div>
+            <div className="card__item-like">{character.gender}</div>
+          </div>
+          <p>{character.location.name}</p>
+          <p>{character.species}</p>
+          <p>{new Date(character.created).toDateString()}</p>
+          <span className="close__modal" onClick={() => props.setOpen(false)}></span>
+        </div>
+      ) : (
+        <div>
+          <strong>{error ? error : <Preloader />}</strong>
+        </div>
+      )}
     </div>
   );
 };
