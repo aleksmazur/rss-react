943e40f84c2b18391760bc0f18d5e74586337ac3
Aleksandra <markovasasha94@gmail.com>
2023-04-09 23:56:21 +0300

    fix: fix search panel test

--- src/components/searchPanel/SearchPanel.test.tsx
+++ src/components/searchPanel/SearchPanel.test.tsx
@@ -1,14 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import SearchPanel from './SearchPanel';
+const mockFn = vi.fn();
+
+const IProps = {
+  setData: mockFn,
+  setInputText: mockFn,
+  inputText: '',
+};
 
 it('search panel has input', () => {
-  render(<SearchPanel />);
+  render(<SearchPanel {...IProps} />);
   const input = screen.getByRole('textbox');
   expect(input).toBeInTheDocument();
 });
 
 it('search panel has input value', () => {
-  render(<SearchPanel />);
+  render(<SearchPanel {...IProps} />);
   const input = screen.getByDisplayValue('');
   expect(input).toBeNull;
 });
